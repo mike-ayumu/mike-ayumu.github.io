d7ea2ae5d0b7e2a280dfd13a2e019c0217ca3be0
Ayumu <ths40374@gmail.com>
2020-03-09 16:04:23 +0900

    Update lp.js

--- staging/js/lp.js
+++ staging/js/lp.js
@@ -36,6 +36,7 @@ document.getElementById('click').onclick = function() {
         }
     };
     viewStorage();
+    viewCookie();
     
     let fromActionList = "";
     let formTag = document.getElementsByTagName('form');
@@ -46,3 +47,42 @@ document.getElementById('click').onclick = function() {
     alert("クッキー\n" + document.cookie);
     alert("フォームアクションURL\n" + fromActionList);
 };
+
+
+
+
+
+function　viewCookie(){
+        var tb = document.getElementById("c-tb")
+        var cookies = getCookieArray();
+        // テーブルの初期化
+        while (tb.firstChild){
+            tb.removeChild(tb.firstChild);
+        }
+        // テーブルの出力
+        for (var i=0; i < cookies; i++) {
+            var _key = localStorage.key(i);
+            var tr = document.createElement("tr");
+            var td1 = document.createElement("td");
+            var td2 = document.createElement("td");
+            tb.appendChild(tr);
+            tr.appendChild(td1);
+            tr.appendChild(td2);
+            td1.innerHTML = i;
+            td2.innerHTML = cookies[i];
+        }
+};
+
+
+//cookie値を連想配列として取得する
+function getCookieArray(){
+    var arr = new Array();
+    if(document.cookie != ''){
+        var tmp = document.cookie.split('; ');
+        for(var i=0;i<tmp.length;i++){
+            var data = tmp[i].split('=');
+            arr[data[0]] = decodeURIComponent(data[1]);
+        }
+    }
+    return arr;
+}
